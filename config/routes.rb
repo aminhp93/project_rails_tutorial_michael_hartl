573ca7bb9525f7076676ef115466270fcb9c34e4
aminhp93 <minhpn.org.ec@gmail.com>
2016-07-24 14:37:02 -0700

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  get 'users/new'
 
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new', as: :signup_path
+
+  resources :users
   
 
   # The priority is based upon order of creation: first created -> highest priority.
